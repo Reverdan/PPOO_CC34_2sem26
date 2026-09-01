fd4e2646a369d599adda59e17a81f67f69d06dd5
Reverdan Almeida Sparinger <reverdan@gmail.com>
2026-09-01 20:06:26 -0300

    PessoaDAO completa

--- CRUDPessoasMaven/src/main/java/com/mycompany/crudpessoasmaven/DAL/PessoaDAO.java
+++ CRUDPessoasMaven/src/main/java/com/mycompany/crudpessoasmaven/DAL/PessoaDAO.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PessoaDAO
@@ -124,11 +125,75 @@ public class PessoaDAO
 
     public void excluirPessoa(Pessoa pessoa)
     {
+        Connection conn = conexao.conectar();
 
+        if (conn != null)
+        {
+            String sql = "DELETE FROM Pessoas WHERE id = ?";
+
+            try (PreparedStatement stmt = conn.prepareStatement(sql))
+            {
+                stmt.setInt(1, pessoa.getId());
+
+                int linhasAfetadas = stmt.executeUpdate();
+
+                if (linhasAfetadas > 0)
+                {
+                    conexao.mensagem = "Pessoa excluída com sucesso.";
+                }
+                else
+                {
+                    conexao.mensagem = "Falha ao excluir a pessoa. Nenhuma linha foi afetada ou o ID não foi encontrado.";
+                }
+            }
+            catch (SQLException e)
+            {
+                conexao.mensagem = "Erro ao executar a operação no banco de dados: " + e.getMessage();
+            }
+            finally
+            {
+                conexao.desconectar();
+            }
+        }
     }
 
     public List<Pessoa> pesquisarPessoaporNome(Pessoa pessoa)
     {
-        return null;
+        Connection conn = conexao.conectar();
+        List<Pessoa> listaPessoas = new ArrayList<>();
+
+        if (conn != null)
+        {
+            String sql = "SELECT id, nome, rg, cpf FROM Pessoas WHERE nome LIKE ?";
+
+            try (PreparedStatement stmt = conn.prepareStatement(sql))
+            {
+                stmt.setString(1, "%" + pessoa.getNome() + "%");
+
+                try (ResultSet rs = stmt.executeQuery())
+                {
+                    while (rs.next())
+                    {
+                        Pessoa p = new Pessoa();
+                        p.setId(rs.getInt("id"));
+                        p.setNome(rs.getString("nome"));
+                        p.setRg(rs.getString("rg"));
+                        p.setCpf(rs.getString("cpf"));
+
+                        listaPessoas.add(p);
+                    }
+                }
+            }
+            catch (SQLException e)
+            {
+                conexao.mensagem = "Erro ao pesquisar pessoas por nome: " + e.getMessage();
+            }
+            finally
+            {
+                conexao.desconectar();
+            }
+        }
+
+        return listaPessoas;
     }
 }
