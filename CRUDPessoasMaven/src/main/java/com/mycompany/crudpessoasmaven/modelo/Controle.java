3ba252f3b95e40a720e9e10d72129c659c90c4a9
Reverdan Almeida Sparinger <reverdan@gmail.com>
2026-09-01 21:34:05 -0300

    Metodos controle

--- CRUDPessoasMaven/src/main/java/com/mycompany/crudpessoasmaven/modelo/Controle.java
+++ CRUDPessoasMaven/src/main/java/com/mycompany/crudpessoasmaven/modelo/Controle.java
@@ -1,6 +1,39 @@
 package com.mycompany.crudpessoasmaven.modelo;
 
+import java.util.List;
+
 public class Controle
 {
+    private String mensagem;
+    
+    public void cadastrarPessoa(List<String> listaDadosPessoa)
+    {
+        
+    }
+    
+    public Pessoa pesquisarPessoa(String numId) 
+    {
+        return null;
+    }
+    
+    public void editarPessoa(List<String> listaDadosPessoa)
+    {
+        
+    }
+    
+    public void excluirPessoa(String numId)
+    {
+        
+    }
+    
+    public List<Pessoa> pesquisarPessoaPorNome(String nome)
+    {
+        return null;
+    }
+
+    public String getMensagem()
+    {
+        return mensagem;
+    }
     
 }
